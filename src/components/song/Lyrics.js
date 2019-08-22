9333627fe5214f9f89900ce581957d64d222a9d6
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-08-22 16:45:08 -0700

    css styling so lyrics are broken up into paragraphs

--- src/components/song/Lyrics.js
+++ src/components/song/Lyrics.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Lyrics.css';
 
 function Lyrics({ artist, title, lyrics }) {
   return (
-    <section>
+    <section className={styles.Lyrics}>
       <h3>{title}</h3>
       <p>by {artist}</p>
       <p>{lyrics ? lyrics : 'This song suxs it has no lyrics!'}</p>
